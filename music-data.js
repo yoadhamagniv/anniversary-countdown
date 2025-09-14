7b217b67d1be60db3e1578109042d60c981c904b
yoad <yoad.abels@gmail.com>
2025-09-14 22:32:28 +0300

    Add album artwork display to music game - shows placeholder initially, reveals cover art when guessed correctly

--- music-data.js
+++ music-data.js
@@ -6,51 +6,61 @@ const musicData = [
     {
         title: "Song 1",
         audioFile: "audio/song1.mp3",
-        artist: "Artist 1"
+        artist: "Artist 1",
+        albumArt: "audio/album1.jpg"
     },
     {
         title: "Song 2", 
         audioFile: "audio/song2.mp3",
-        artist: "Artist 2"
+        artist: "Artist 2",
+        albumArt: "audio/album2.jpg"
     },
     {
         title: "Song 3",
         audioFile: "audio/song3.mp3", 
-        artist: "Artist 3"
+        artist: "Artist 3",
+        albumArt: "audio/album3.jpg"
     },
     {
         title: "Song 4",
         audioFile: "audio/song4.mp3",
-        artist: "Artist 4"
+        artist: "Artist 4",
+        albumArt: "audio/album4.jpg"
     },
     {
         title: "Song 5",
         audioFile: "audio/song5.mp3",
-        artist: "Artist 5"
+        artist: "Artist 5",
+        albumArt: "audio/album5.jpg"
     },
     {
         title: "Song 6",
         audioFile: "audio/song6.mp3",
-        artist: "Artist 6"
+        artist: "Artist 6",
+        albumArt: "audio/album6.jpg"
     },
     {
         title: "Song 7",
         audioFile: "audio/song7.mp3",
-        artist: "Artist 7"
+        artist: "Artist 7",
+        albumArt: "audio/album7.jpg"
     },
     {
         title: "Song 8",
         audioFile: "audio/song8.mp3",
-        artist: "Artist 8"
+        artist: "Artist 8",
+        albumArt: "audio/album8.jpg"
     },
     {
         title: "Song 9",
         audioFile: "audio/song9.mp3",
-        artist: "Artist 9"
+        artist: "Artist 9",
+        albumArt: "audio/album9.jpg"
     },
     {
         title: "Song 10",
         audioFile: "audio/song10.mp3",
-        artist: "Artist 10"
+        artist: "Artist 10",
+        albumArt: "audio/album10.jpg"
     }
 ];
