bf3bcc3651d3667339a8346763fd242fd5cdff55
yoad <yoad.abels@gmail.com>
2025-09-14 22:20:34 +0300

    Fix memory game grid layout - corrected CSS selector from .memory-grid to #memoryGrid

--- memory-data.js
+++ memory-data.js
@@ -7,7 +7,7 @@ const memoryMatchData = [
         part1: "images/mm1_part1.jpg",
         part2: "images/mm1_part2.jpg", 
         full: "images/mm1_full.jpg",
-        description: "Our first date - coffee and conversation",
+        description: "At your grandparents' house",
         loveMessage: "This is where it all began. That nervous smile, those first laughs, the moment I knew you were special."
     },
     {
@@ -15,7 +15,7 @@ const memoryMatchData = [
         part1: "images/mm2_part1.jpg",
         part2: "images/mm2_part2.jpg",
         full: "images/mm2_full.jpg", 
-        description: "That perfect sunset we watched together",
+        description: "Long awaited bathroom break on the way to Haifa",
         loveMessage: "Golden hour with you is my favorite time of day. The way the light catches your eyes takes my breath away."
     },
     {
@@ -23,7 +23,7 @@ const memoryMatchData = [
         part1: "images/mm3_part1.jpg",
         part2: "images/mm3_part2.jpg",
         full: "images/mm3_full.jpg",
-        description: "Dancing in the kitchen like no one's watching", 
+        description: "Making your dreams come true", 
         loveMessage: "These spontaneous moments are what I live for. Your laugh echoing through our home is my favorite sound."
     },
     {
@@ -31,7 +31,7 @@ const memoryMatchData = [
         part1: "images/mm4_part1.jpg",
         part2: "images/mm4_part2.jpg",
         full: "images/mm4_full.jpg",
-        description: "The moment I knew you were the one",
+        description: "Visiting your fam in Natanya",
         loveMessage: "Every day with you confirms what I felt that day. You're not just my girlfriend, you're my best friend and my future."
     },
     {
@@ -39,7 +39,7 @@ const memoryMatchData = [
         part1: "images/mm5_part1.jpg",
         part2: "images/mm5_part2.jpg",
         full: "images/mm5_full.jpg",
-        description: "That time we got lost but found each other",
+        description: "Having weird ice cream at the shuk",
         loveMessage: "Even when we're lost, being with you feels like home. You're my compass, my anchor, my everything."
     },
     {
@@ -47,7 +47,7 @@ const memoryMatchData = [
         part1: "images/mm6_part1.jpg",
         part2: "images/mm6_part2.jpg",
         full: "images/mm6_full.jpg",
-        description: "Laughing until our stomachs hurt",
+        description: "picking up Gili from tennis practice",
         loveMessage: "Your laugh is my favorite melody. I could listen to it forever and never get tired of the music it makes."
     },
     {
@@ -55,7 +55,7 @@ const memoryMatchData = [
         part1: "images/mm7_part1.jpg",
         part2: "images/mm7_part2.jpg",
         full: "images/mm7_full.jpg",
-        description: "The way you look at me when you think I'm not looking",
+        description: "Munching on some Shawarma",
         loveMessage: "I see the love in your eyes even when you think I don't. It's the most beautiful thing I've ever witnessed."
     },
     {
@@ -63,7 +63,7 @@ const memoryMatchData = [
         part1: "images/mm8_part1.jpg",
         part2: "images/mm8_part2.jpg",
         full: "images/mm8_full.jpg",
-        description: "Our first adventure together",
+        description: "Visiting my Iranian fam in Rishon Lezion",
         loveMessage: "Every adventure is better with you by my side. You make the ordinary extraordinary and the impossible possible."
     },
     {
@@ -71,7 +71,7 @@ const memoryMatchData = [
         part1: "images/mm9_part1.jpg",
         part2: "images/mm9_part2.jpg",
         full: "images/mm9_full.jpg",
-        description: "That perfect morning we never wanted to end",
+        description: "You being crazy as per usual",
         loveMessage: "Mornings with you are my favorite part of the day. Waking up next to you is like waking up to sunshine."
     },
     {
@@ -79,7 +79,7 @@ const memoryMatchData = [
         part1: "images/mm10_part1.jpg",
         part2: "images/mm10_part2.jpg",
         full: "images/mm10_full.jpg",
-        description: "The moment I fell in love with you all over again",
+        description: "After the beach at my cousins' house",
         loveMessage: "I fall in love with you more every single day. You're not just my past and present, you're my forever future."
     }
 ];
